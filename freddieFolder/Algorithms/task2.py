7e44c12ea8a0866ce8c3a0b158f92e7e39c744e0
Freddie-7 <164356679+Freddie-7@users.noreply.github.com>
2024-03-22 21:42:51 +0000

    Add files via upload

---
+++ freddieFolder/Algorithms/task2.py
@@ -0,0 +1,33 @@
+import math
+import Projectile
+import Constants
+
+
+def calculateTrajectoryData(proj: Projectile.Projectile):
+    """
+    A procedure that takes in a projectile object and runs it through a drag-less projectile motion using the projectile equation (analytic method).
+    It keeps increasing x regularly until it hits the floor (y < 0).
+    :param proj: Projectile.Projectile
+    :return: None
+    """
+
+    startingHeight = proj.y   # save starting height, as changes during algorithm
+    #  \/ Then use formula to calculate maximum range
+    maxRange = (proj.initialVel**2/Constants.g) * (math.sin(proj.initialAngle) * math.cos(proj.initialAngle) + math.cos(proj.initialAngle) * math.sqrt(math.sin(proj.initialAngle) ** 2 + (2 * Constants.g * startingHeight) / proj.initialVel ** 2))
+    deltaX = maxRange * Constants.FRACTION_OF_RANGE  # defined the amount that x is increasing per update
+    for step in range(int(1/Constants.FRACTION_OF_RANGE)):  # Loop that moves through the updates
+        proj.x += deltaX
+        proj.y = startingHeight + proj.x * math.tan(proj.initialAngle) - (Constants.g / (2 * proj.initialVel**2)) * (1 + math.tan(proj.initialAngle)**2) * proj.x ** 2  # formula for y position, based off of x position of a projectile.
+        proj.log(True)  # True to make the procedure record the range fraction
+
+    # x and y coordinated for the maximum point that the projectile gets to, for later use.
+    xApogee = (proj.initialVel ** 2 / Constants.g) * math.sin(proj.initialAngle) * math.cos(proj.initialAngle)
+    yApogee = startingHeight + (proj.initialVel ** 2 / (Constants.g * 2)) * math.sin(proj.initialAngle) ** 2
+    print('x Apogee: ', xApogee)
+    print('y Apogee: ', yApogee, '\n')
+
+
+if __name__ == '__main__':
+    projectile = Projectile.Projectile(x=0, y=1, v=10, angle=42)  # add parameters (x=_, y=_, v=_, angle=_) to change default values
+    calculateTrajectoryData(projectile)  # run through projectile motion
+    projectile.printAsTable(columns=('rangeFraction', 'x', 'y'))
